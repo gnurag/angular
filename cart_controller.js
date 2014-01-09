7635d14ae763b44652e337efe5a47c95e460f4fd
Daniel Eom <danieljoonyongum@gmail.com>
2014-01-09 14:42:02 +0900

    Second Work for Angular

--- cart_controller.js
+++ cart_controller.js
@@ -1,3 +1,26 @@
 /**
  * Created by Daniel on 1/6/14.
  */
+function CartController($scope) {
+    $scope.items = [
+        {
+            title: 'bucket of paint ',
+            quantity: 8,
+            price: 3.95
+        } ,
+        {
+            title: "ribbon",
+            quantity: 10,
+            price: 9.95
+        },
+        {
+            title: "ball",
+            quantity: 100,
+            price: 10
+        }
+    ];
+    $scope.remove = function (index) {
+        alert('index = '+ index) ;
+        $scope.items.splice(index, 1);
+    }
+}
--- controllers.js
+++ controllers.js
@@ -1,3 +1,7 @@
 /**
  * Created by Daniel on 1/6/14.
  */
+function HelloController($scope) {
+    $scope.greeting = {
+        text: 'Hello' };
+}
